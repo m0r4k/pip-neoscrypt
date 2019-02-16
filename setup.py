4cfa5539e05c087dcee52ec05a55b90cfe8495c1
m0r4k <mor4k@outlook.com>
2019-02-16 13:55:37 +0100

    upadte setup

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup, Extension
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 neoscrypt_module = Extension('neoscrypt',
                              sources=['neoscrypt/neoscryptmodule.c',
                                       'neoscrypt/neoscrypt.c'],
                              include_dirs=['.', 'neoscrypt'])
 
-setup (name = 'neoscrypt',
-       version = '1.2',
-       description = 'Bindings for the NeoScrypt proof-of-work algorithm',
-       author = 'John Doering',
-       author_email = 'ghostlander@phoenixcoin.org',
-       url = 'https://github.com/ghostlander/NeoScrypt, ',
-       ext_modules = [neoscrypt_module],
-       zip_safe = True)
+setup(name='neoscrypt',
+      version='1.2',
+      description='Bindings for the NeoScrypt proof-of-work algorithm',
+      author='John Doering',
+      author_email='ghostlander@phoenixcoin.org',
+      url='https://github.com/ghostlander/NeoScrypt, ',
+      ext_modules=[neoscrypt_module],
+      zip_safe=True,
+      long_description=long_description,
+      )
